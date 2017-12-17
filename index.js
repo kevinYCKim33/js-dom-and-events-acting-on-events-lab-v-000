09b2a3a0c97682da9456cc9d04f850f4152dbeb7
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-16 18:22:52 -0800

    basic rock dodger sliding implemented

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+let dodger = $('#dodger')[0];
+
+dodger.style.backgroundColor = '#FF69B4';
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  //'100px' => '100'
+  let left = parseInt(leftNumbers, 10)
+  // '100' => 100
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '')
+  //'100px' => '100'
+  let left = parseInt(leftNumbers, 10)
+  // '100' => 100
+  if (left < 360 ) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  } else if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
